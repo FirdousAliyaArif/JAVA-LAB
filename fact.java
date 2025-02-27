e17996adc12700d01fc0d4b8dded765a705a9476
Firdous Aliya Arif <frdsaliya@gmail.com>
2025-02-27 11:14:49 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,12 @@
+class fact
+{
+      public static void main(String[] ar)
+      {  
+             int a=4,f=1;
+             for(int i=1; i<=a; i++)
+             {
+                 f=f*i;
+             }
+             System.out.println("Factorial of "+a+" is "+f);
+      }
+}
